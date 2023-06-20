9c2f38ada4f7cbb4a3c4703cc8a14f74bacf2509
DanielMoreiraCoelho <93950874+DanielMoreiraCoelho@users.noreply.github.com>
2023-06-20 18:54:06 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,14 @@ require("@firebase/firestore");
 
 var firebaseConfig = {
   //Cole sua Configuração do Firebase aqui
-  
+  const firebaseConfig = {
+    apiKey: "AIzaSyDOm0xj5_sI0Bg19eJ2q8DsBxL8RUvRn4E",
+    authDomain: "projeto-c71-1323d.firebaseapp.com",
+    projectId: "projeto-c71-1323d",
+    storageBucket: "projeto-c71-1323d.appspot.com",
+    messagingSenderId: "821969701573",
+    appId: "1:821969701573:web:804d3cef923686b7efbeac"
+  };
 };
 
 firebase.initializeApp(firebaseConfig);
